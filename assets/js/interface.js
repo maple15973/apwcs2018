903a2ecfe9e5fe8028f361642dba7f7579716bc5
Cober <maple15973@gmail.com>
2018-02-11 13:36:29 +0800

    first commit

---
+++ assets/js/interface.js
@@ -0,0 +1,147 @@
+﻿( function($) {
+  'use strict';
+
+$(function(e) {
+	
+	/*-------------------------------------------------------------------------------
+	  Smooth scroll to anchor
+	-------------------------------------------------------------------------------*/
+	var navbar=$('.js-navbar');
+	var navbarAffixHeight=90
+	$('.js-target-scroll').on('click', function(e) {
+		var target = $(this.hash);
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: (target.offset().top - navbarAffixHeight + 1)
+			}, 1000);
+			return false;
+		}
+	});
+	$('body').scrollspy({
+			offset:  navbarAffixHeight + 1
+	});
+	$('.navbar-nav a ').on('click', function(){
+	  if($('#navigation').removeClass('shownav')) {
+	  }
+    });
+	
+	/*-------------------------------------------------------------------------------
+		Navigation
+	-------------------------------------------------------------------------------*/
+	$('#menu-toggle-bar').on('click', function(e) {
+		   $("#navigation").toggleClass("shownav");
+	});
+	$('#menu-toggle-close').on('click', function(e) {
+		   $("#navigation").removeClass("shownav");
+	});
+
+	/*-------------------------------------------------------------------------------
+	 Timer
+	-------------------------------------------------------------------------------*/
+ 	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+	$('#defaultCountdown').countdown({until: austDay});
+	$('#year').text(austDay.getFullYear());
+ 
+	/*-------------------------------------------------------------------------------
+	  Tab li Add Class
+	-------------------------------------------------------------------------------*/
+$('.conf-close').on('click', function(e) {
+	if ( $(this).hasClass('menu-down') ) {
+		$(this).removeClass('menu-down');
+	} else {
+		$(this).addClass('menu-down');    
+	}
+});	
+	
+/*-------------------------------------------------------------------------------
+  Background slider
+-------------------------------------------------------------------------------*/
+   
+  $("#slideshow").owlCarousel({
+	autoPlay : 5000,
+	stopOnHover : false,
+	navigation:false,
+	paginationSpeed : 1000,
+	goToFirstSpeed : 2000,
+	singleItem : true,
+	autoHeight : true,
+	transitionStyle:"fadeUp"
+  });
+ 
+ 
+ /*------------------------------------------------------------------
+	Countdown
+	-------------------------------------------------------------------*/
+ 
+	var endDate = "august 21, 2018";
+	$('.countdown.styled').countdown({
+	  date: endDate,
+	  render: function(data) {
+		$(this.el).html("<div class='countdown-amount'>" + this.leadingZeros(data.days, 2) + " <span class='countdown-period'>Days</span></div><div class='countdown-amount'>" + this.leadingZeros(data.hours, 2) + " <span class='countdown-period'>Hours</span></div><div class='countdown-amount'>" + this.leadingZeros(data.min, 2) + " <span class='countdown-period'>Minutes</span></div><div class='countdown-amount'>" + this.leadingZeros(data.sec, 2) + " <span class='countdown-period'>Seconds</span></div>");
+	  }
+	});	
+  
+  
+  /*------------------------------------------------------------------
+	back to top
+	-------------------------------------------------------------------*/
+ var top = $('#back-top');
+	top .hide();
+	 
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				top .fadeIn();
+			} else {
+				top .fadeOut();
+			}
+		});
+		$('#back-top a').on('click', function(e) {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	 
+ /*-------------------------------------------------------------------------------
+  Ajax Form
+	-------------------------------------------------------------------------------*/
+ 
+	if ($('#js-ajax-form').length) {
+		$('#js-ajax-form').each(function(){
+			$(this).validate({
+				errorClass: 'error wobble-error',
+				submitHandler: function(form){
+					$.ajax({
+						type: "POST",
+						url:"mail.php",
+						data: $(form).serialize(),
+						success: function() {
+							$('.modal').modal('hide');
+							$('#success').modal('show');
+						},
+	
+						error: function(){
+							$('.modal').modal('hide');
+							$('#error').modal('show');
+						}
+					});
+				}
+			});
+		});
+	}
+/*-------------------------------------------------------------------------------
+  Paypal Form
+	-------------------------------------------------------------------------------*/
+	
+	if ($('#paypal-form').length) {
+		$('#paypal-form').each(function(){
+			$(this).validate({
+				errorClass: 'error wobble-error',
+			});
+		});
+	}	
+ 
+});
+
+})(jQuery);
